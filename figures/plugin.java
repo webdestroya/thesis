d7b72c2b836ea4ca7f4107f5d59c31ae4ac545fc
Mitch Dempsey <mrdempsey@gmail.com>
2012-10-14 17:41:05 -0700

    Adding Implementation updates

---
+++ figures/plugin.java
@@ -0,0 +1,17 @@
+public class ThesisPlugin extends AbstractCalicoPlugin implements CalicoPlugin {
+  public ThesisPlugin() {
+    super();
+    PluginInfo.name = "ThesisPlugin";
+    PluginInfo.author = "mdempsey";
+    PluginInfo.info = "Thesis Demonstration Plugin";
+    PluginInfo.url = "http://ics.uci.edu/~mdempsey/";
+  }
+  
+  public void onPluginStart() {
+    RegisterPluginEvent(calico.plugins.designminders.DMScrap2Card.class);
+  }
+  
+  public void onPluginEnd() {
+    // this code will be run on shutdown
+  }
+}

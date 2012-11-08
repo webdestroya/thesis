11a6b51b7d8a0687e2ecfa7277ce05b950feaf85
Mitch Dempsey <mrdempsey@gmail.com>
2012-11-07 18:16:47 -0800

    Updates

---
+++ diff.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+latexdiff-git $1 thesis.tex chapters/introduction.tex chapters/background.tex chapters/calico.tex chapters/conclusion.tex chapters/high_level_architecture.tex chapters/implementation.tex chapters/objectives.tex chapters/implementation/admin_ui.tex chapters/implementation/input_handling.tex chapters/implementation/networking.tex chapters/implementation/object_controllers.tex chapters/implementation/object_storage.tex chapters/implementation/plugin_framework.tex
+
+
+
+mv thesis-diff.pdf output/
+rm texput.log thesis-diff.*
+
+open output/thesis-diff.pdf
